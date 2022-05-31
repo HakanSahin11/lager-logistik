ef0f56c69b4fabe1fa673438fdc6b0d8aafd73c6
Karsten Reitan Sørensen <karsten.reitan@gmail.com>
2022-05-31 11:04:00 +0200

    Init commit

---
+++ frontend/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import home
+
+''' The function of this file is to define which view will be accessed by a user if 
+    they access a certain URL. In this case, when they access the root URL, they will 
+    be directed to the home view.'''
+urlpatterns = [
+    path('', home,name='home'),
+]
