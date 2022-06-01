fac6e3c0cda44cbefe355645bef147b848f40979
Karsten Reitan Sørensen <karsten.reitan@gmail.com>
2022-06-01 11:23:51 +0200

    Init commit

--- frontend/models.py
+++ frontend/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 # Create your models here.
 class PostItem(models.Model):
-    name =  models.CharField(max_length=255) # 
+    name =  models.CharField(max_length=255) # Svarer til feltet Navn
